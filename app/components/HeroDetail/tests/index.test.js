387854454e42df6a3f7153b3d9c32635e6e51e20
M4G1C3LF <sbalaguercarmona@gmail.com>
2019-07-17 13:44:31 +0200

    Create tests for Hero Detail.

--- app/components/HeroDetail/tests/index.test.js
+++ app/components/HeroDetail/tests/index.test.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import ListItem from '../index';
+import HeroDetail from '../index';
 
 describe('<ListItem />', () => {
-  it('should have a className', () => {
-    const renderedComponent = mount(<ListItem className="test" />);
-    expect(renderedComponent.find('li').prop('className')).toBeDefined();
+  it('should render an error', () => {
+    const renderedComponent = mount(<HeroDetail />);
+    const content = <div><p>Error loading hero detail</p></div>;
+    expect(renderedComponent.contains(content)).toBe(true);
   });
 
-  it('should render the content passed to it', () => {
-    const content = <div>Hello world!</div>;
-    const renderedComponent = mount(<ListItem item={content} />);
-    expect(renderedComponent.contains(content)).toBe(true);
+  it('should render the HeroDetail', () => {
+    const renderedComponent = mount(
+      <HeroDetail
+        id={1}
+        name="Mariano Rajoy Brey"
+        thumbnail="https://st-listas.20minutos.es/images/2012-04/328417/3499469_640px.jpg?1337178623"
+        age={64}
+        weight={176.3345}
+        height={80.45453}
+        hairColor="Pink"
+        professions={['Politician', 'Runner', 'Swimmer']}
+        friends={['Soraya Saez de Santamaría', 'Francisco Correa', 'Esperanza Aguirre', 'Jose María Aznar']}
+      />
+    );
+
+    const content = <div><p>Error loading hero detail</p></div>;
+    expect(renderedComponent.contains(content)).toBe(false);
+  });
+
+  it("should render the HeroDetail, using id 0 (it's a falsy)", () => {
+    const renderedComponent = mount(
+      <HeroDetail
+        id={0}
+        name="Mariano Rajoy Brey"
+        thumbnail="https://st-listas.20minutos.es/images/2012-04/328417/3499469_640px.jpg?1337178623"
+        age={64}
+        weight={176.3345}
+        height={80.45453}
+        hairColor="Pink"
+        professions={['Politician', 'Runner', 'Swimmer']}
+        friends={['Soraya Saez de Santamaría', 'Francisco Correa', 'Esperanza Aguirre', 'Jose María Aznar']}
+      />
+    );
+
+    const content = <div><p>Error loading hero detail</p></div>;
+    expect(renderedComponent.contains(content)).toBe(false);
   });
 });

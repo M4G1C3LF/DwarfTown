967266415e9bbc29fc2aa5e4947710df3e8dba34
M4G1C3LF <sbalaguercarmona@gmail.com>
2019-07-17 18:50:57 +0200

    Create HomePage.

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -9,16 +9,33 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import ReposList from 'components/ReposList';
 import './style.scss';
+import HeroDetail from 'components/HeroDetail';
+import HeroList from 'components/HeroList';
+import HeroListMock from 'components/HeroList/mock';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.selectHero = this.selectHero.bind(this);
+  }
+
   /**
    * when initial state username is not null, submit the form to load repos
    */
+
+  state = {
+    selectedHero: null
+  }
+
   componentDidMount() {
     const { username, onSubmitForm } = this.props;
     if (username && username.trim().length > 0) {
       onSubmitForm();
     }
+  }
+
+  selectHero(hero) {
+    this.setState({ selectedHero: hero });
   }
 
   render() {
@@ -31,38 +48,27 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
       repos
     };
 
+    const { selectedHero } = this.state;
+
     return (
-      <article>
-        <Helmet>
-          <title>Home Page</title>
-          <meta name="description" content="A React.js Boilerplate application homepage" />
-        </Helmet>
-        <div className="home-page">
-          <section className="centered">
-            <h2>Start your next react project in seconds</h2>
-            <p>
-              A minimal <i>React-Redux</i> boilerplate with all the best practices
-            </p>
-          </section>
-          <section>
-            <h2>Try me!</h2>
-            <form onSubmit={onSubmitForm}>
-              <label htmlFor="username">
-                Show Github repositories by
-                <span className="at-prefix">@</span>
-                <input
-                  id="username"
-                  type="text"
-                  placeholder="flexdinesh"
-                  value={username}
-                  onChange={onChangeUsername}
-                />
-              </label>
-            </form>
-            <ReposList {...reposListProps} />
-          </section>
-        </div>
-      </article>
+      <div>
+        <HeroList heroes={HeroListMock.Brastlewark} onClickItem={this.selectHero} />
+        {selectedHero
+          && (
+            <HeroDetail
+              id={selectedHero.id}
+              name={selectedHero.name}
+              thumbnail={selectedHero.thumbnail}
+              age={selectedHero.age}
+              weight={selectedHero.weight}
+              height={selectedHero.height}
+              hairColor={selectedHero.hair_color}
+              professions={selectedHero.professions}
+              friends={selectedHero.friends}
+            />
+          )
+        }
+      </div>
     );
   }
 }

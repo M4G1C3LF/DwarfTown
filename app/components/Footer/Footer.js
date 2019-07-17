3924336b92cf82a4f088864ddac8efcfb7b589ad
M4G1C3LF <sbalaguercarmona@gmail.com>
2019-07-17 23:20:10 +0200

    HOTFIX: Set Height to 10vh

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './style.scss';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  height: 10vh;
+`;
 
 const Footer = () => (
-  <footer>
+  <Wrapper>
     <section>2019 - Sergio Balaguer Carmona</section>
-  </footer>
+  </Wrapper>
 );
 
 export default Footer;

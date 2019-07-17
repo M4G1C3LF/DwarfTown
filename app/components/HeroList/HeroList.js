67560358b46176f2c8c82ab2aa0800ecaf9e328f
M4G1C3LF <sbalaguercarmona@gmail.com>
2019-07-17 18:47:02 +0200

    Create and pass through props the method onClickItem to each hero on the list.

--- app/components/HeroList/HeroList.js
+++ app/components/HeroList/HeroList.js
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.scss';
 
-const HeroListItem = ({ hero }) => <li>{hero.name}</li>;
+const HeroListItem = ({ hero, onClickItem }) => <li onClick={ () => onClickItem(hero) }>{hero.name}</li>;
 
 HeroListItem.propTypes = {
   hero: PropTypes.object,
+  onClickItem: PropTypes.func,
 };
 
-const HeroList = ({ heroes }) => (
+const HeroList = ({ heroes, onClickItem }) => (
   <div id="HeroList">
     {heroes && heroes.length ? (
       <ul>
-        {heroes.map((hero) => <HeroListItem key={`HeroListItem_${hero.id}`} hero={hero} />)}
-
+        {heroes.map((hero) => <HeroListItem key={`HeroListItem_${hero.id}`} hero={hero} onClickItem={onClickItem}/>)}
       </ul>
     ) : (
       <div>
@@ -27,6 +27,7 @@ const HeroList = ({ heroes }) => (
 
 HeroList.propTypes = {
   heroes: PropTypes.array,
+  onClickItem: PropTypes.func,
 };
 
 export default HeroList;

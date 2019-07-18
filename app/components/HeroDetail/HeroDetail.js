e3438ba32cb60c74ed74cfcec701a7ec8614d902
M4G1C3LF <sbalaguercarmona@gmail.com>
2019-07-18 23:02:50 +0200

    Add Draggable scrolling.

--- app/components/HeroDetail/HeroDetail.js
+++ app/components/HeroDetail/HeroDetail.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import DragScroll from 'react-dragscroll';
+
 import './style.scss';
 
 const DetailItem = ({ label, value }) => (
@@ -48,35 +50,36 @@ const Image = styled.img`
 const HeroDetail = ({
   id, name, thumbnail, age, weight, height, hairColor, professions, friends
 }) => (
-  <Wrapper>
-    {(id || id === 0) && name && age && weight && height && hairColor ? (
-      <div id={`heroDetail_${id}`}>
-        <Image src={thumbnail} alt={`${name}'s thumbnail`} />
-        <DetailItem label="Name" value={name} />
-        <DetailItem label="Age" value={age.toString()} />
-        <DetailItem label="Weight" value={weight.toFixed(2).toString()} />
-        <DetailItem label="Height" value={height.toFixed(2).toString()} />
-        <DetailItem label="Hair Color" value={hairColor} />
-        {professions && <DetailList label="Professions" values={professions} />}
-        {friends && <DetailList label="Friends" values={friends} />}
-      </div>
-    ) : (
-      <div>
-        <p>Error loading hero detail</p>
-        <h4>Props</h4>
-        <p>id: {id}</p>
-        <p>name: {name}</p>
-        <p>thumbnail: {thumbnail}</p>
-        <p>iaged: {age}</p>
-        <p>weight: {weight}</p>
-        <p>height: {height}</p>
-        <p>hairColor: {hairColor}</p>
-        <p>professions: {professions}</p>
-        <p>friends: {friends}</p>
-      </div>
-    )}
-  </Wrapper>
-
+  <DragScroll height={"80vh"} width={"30vw"}>
+    <Wrapper>
+      {(id || id === 0) && name && age && weight && height && hairColor ? (
+        <div id={`heroDetail_${id}`}>
+          <Image src={thumbnail} alt={`${name}'s thumbnail`} />
+          <DetailItem label="Name" value={name} />
+          <DetailItem label="Age" value={age.toString()} />
+          <DetailItem label="Weight" value={weight.toFixed(2).toString()} />
+          <DetailItem label="Height" value={height.toFixed(2).toString()} />
+          <DetailItem label="Hair Color" value={hairColor} />
+          {professions && <DetailList label="Professions" values={professions} />}
+          {friends && <DetailList label="Friends" values={friends} />}
+        </div>
+      ) : (
+        <div>
+          <p>Error loading hero detail</p>
+          <h4>Props</h4>
+          <p>id: {id}</p>
+          <p>name: {name}</p>
+          <p>thumbnail: {thumbnail}</p>
+          <p>iaged: {age}</p>
+          <p>weight: {weight}</p>
+          <p>height: {height}</p>
+          <p>hairColor: {hairColor}</p>
+          <p>professions: {professions}</p>
+          <p>friends: {friends}</p>
+        </div>
+      )}
+    </Wrapper>
+  </DragScroll>
 );
 
 HeroDetail.propTypes = {

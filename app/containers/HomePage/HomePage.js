951beb24574e4d5682914f23dc929573ea78afcc
M4G1C3LF <sbalaguercarmona@gmail.com>
2019-07-21 12:43:31 +0200

    Remove unnecessary code (references to deleted components).

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -6,24 +6,12 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
-import ReposList from 'components/ReposList';
 import './style.scss';
 import HeroDetail from 'components/HeroDetail';
 import HeroList from 'components/HeroList';
 // import HeroListMock from 'components/HeroList/mock';
-import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
-import { filterByName, filterByProfession, getProfessions } from 'utils/filteringTools' 
-const Wrapper = styled.div`
-  display: inline-flex;
-`;
-const HeroListWrapper = styled.div`
-  float: left;
-`;
-const HeroDetailWrapper = styled.div`
-  float: right;
-`;
+import { filterByName, filterByProfession, getProfessions } from 'utils/filteringTools';
 
 export default class HomePage extends React.PureComponent {
   constructor(props) {
@@ -44,29 +32,17 @@ export default class HomePage extends React.PureComponent {
   }
 
   componentDidMount() {
-    const { username, onSubmitForm, onLoadPage } = this.props;
-
+    const { onLoadPage } = this.props;
     onLoadPage();
-    if (username && username.trim().length > 0) {
-      onSubmitForm();
-    }
   }
 
   selectHero(hero) {
     this.setState({ selectedHero: hero });
-    document.getElementById("heroDetailWrapper").scrollIntoView();
+    document.getElementById('heroDetailWrapper').scrollIntoView();
   }
 
   render() {
-    const {
-      loading, error, repos, username, onChangeUsername, onSubmitForm, town
-    } = this.props;
-
-    const reposListProps = {
-      loading,
-      error,
-      repos
-    };
+    const { town } = this.props;
 
     const { selectedHero, filter } = this.state;
     return (
@@ -111,11 +87,6 @@ export default class HomePage extends React.PureComponent {
 }
 
 HomePage.propTypes = {
-  loading: PropTypes.bool,
-  error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-  repos: PropTypes.oneOfType([PropTypes.array, PropTypes.bool]),
-  onSubmitForm: PropTypes.func,
-  username: PropTypes.string,
-  onChangeUsername: PropTypes.func,
+  town: PropTypes.object,
   onLoadPage: PropTypes.func,
 };

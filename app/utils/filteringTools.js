ac1d50a132f1a5aab25fdfe2cef4e72a37e74aba
M4G1C3LF <sbalaguercarmona@gmail.com>
2019-07-21 11:58:57 +0200

    Create method "GetProfessions" and "FilterByProfessions"

--- app/utils/filteringTools.js
+++ app/utils/filteringTools.js
@@ -2,3 +2,28 @@ export function filterByName(characters, filterString) {
   const result = characters && characters.length ? characters.filter((character) => character.name.toLowerCase().includes(filterString.toLowerCase())) : null;
   return result;
 }
+
+export function filterByProfession(characters, professionFilter) {
+  console.log("FilterByProfession")
+  console.log(JSON.stringify(characters));
+  if (!professionFilter) return characters;
+  const result = characters && characters.length ? characters.filter((character) => character.professions.includes(professionFilter)) : null;
+  return result;
+}
+
+export function getProfessions(characters) {
+  const jobs = [];
+
+  if (characters && characters.length) {
+    characters.map((char) => {
+      if (char.professions && char.professions.length) {
+        char.professions.map((job) => {
+          if (!jobs.includes(job)) jobs.push(job);
+          return null;
+        });
+      }
+      return null;
+    });
+  }
+  return jobs;
+}
